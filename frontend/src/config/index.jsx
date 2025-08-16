a42f5b3e9cc53223bfe6b7241a960f24b8aceacd
Sujit Thakur <tsujeet440@gmail.com>
2025-08-16 18:20:30 +0530

    Readme Updated

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,8 @@
 const { default: axios } = require("axios");
 
-export const BASE_URL =  "https://concretion.onrender.com";
+// export const BASE_URL =  "https://concretion.onrender.com";
+export const BASE_URL = "http://localhost:5500"
+
 
 export const clientServer = axios.create({
   baseURL: BASE_URL,
